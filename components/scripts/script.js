15c696d8c8c2c0a067c9d3158ed385452b8501f0
Mark Robison <ballaboy@gmail.com>
2015-08-11 18:49:21 -0400

    Inital Commit - Adding HTML, CSS, JavaScript in first Form

---
+++ components/scripts/script.js
@@ -0,0 +1,101 @@
+$(function() {
+
+    var topoffset = 43;
+
+    var wheight =$(window).height(); //get height of window using jquery
+
+      $('.fullheight').css('height', wheight);
+
+      $(window).resize(function() {
+            var wheight =$(window).height(); //get height of window using jquery
+            $('.fullheight').css('height', wheight);
+      }) //on resize
+
+
+
+  var isTouch = 'ontouchstart' in document.documentElement;
+
+  //window height
+  var wheight = $(window).height(); //get height of the window
+
+  $('.fullheight').css('height', wheight);
+
+  $(window).resize(function() {
+    var wheight = $(window).height(); //get height of the window
+    $('.fullheight').css('height', wheight);
+  }) //on resize
+
+
+// Animated Scrolling
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top-topoffset
+        }, 1000);
+        return false;
+      } // target.length
+    } //location hostname
+  }); //on click
+
+  //highlight navigation
+  $(window).scroll(function() {
+    var windowpos = $(window).scrollTop() + topoffset;
+    $('nav li a').removeClass('active');
+
+    if (windowpos > $('#welcome').offset().top) {
+      $('nav li a').removeClass('active');
+      $('a[href$="#welcome"]').addClass('active');
+    } //windowpos
+
+    if (windowpos > $('#events').offset().top) {
+      $('nav li a').removeClass('active');
+      $('a[href$="#events"]').addClass('active');
+    } //windowpos
+
+    if (windowpos > $('#ministries').offset().top) {
+      $('nav li a').removeClass('active');
+      $('a[href$="#ministries"]').addClass('active');
+    } //windowpos
+
+    if (windowpos > $('#about').offset().top) {
+      $('nav li a').removeClass('active');
+      $('a[href$="#about"]').addClass('active');
+    } //windowpos
+
+    if (windowpos > $('#staff').offset().top) {
+      $('nav li a').removeClass('active');
+      $('a[href$="#staff"]').addClass('active');
+    } //windowpos
+
+  }); //window scroll
+
+
+
+
+  //set up ScrollMagic
+  var controller = new ScrollMagic({
+    globalSceneOptions: {
+      triggerHook: "onLeave"
+    }
+  });
+
+  //pin the navigation
+  var pin = new ScrollScene({
+    triggerElement: '#nav',
+  }).setPin('#nav').addTo(controller);
+
+
+
+}); //on load
+
+    //colorbox
+    $('a.btn_map, a.btn_cal').colorbox({
+        iframe: true, width:800, height:600, maxWidth:"99%", maxHeight:"95%", opacity:0.7
+        }); //btn_map, cal
+	
+	$('a.btn_light, a.btn_dark, a.pic_btn').colorbox({
+        iframe: true, width: 800, height:600, maxWidth:"99%", maxHeight:"95%", opacity:0.7
+        }); //btn_light, dark, pic
