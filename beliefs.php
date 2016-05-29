62fcf9566b3390d6c50306d1bf2d76addf364cbe
Mark <mrobison@blackstonemedia.com>
2016-05-28 21:59:26 -0400

    header working but not arrow

---
+++ beliefs.php
@@ -0,0 +1,168 @@
+<div class="slides-container slides-container--fade row" data-active-tab="2">
+				<ul class="slides-nav" role="tablist">
+											<h3 class="slides-title prev">Our Beliefs</h3>
+
+					
+					<li class="active" role="tab">
+						<a href="#slide1"></a>
+						<span>God</span>
+					</li>
+				
+					<li class="next" role="tab">
+						<a href="#slide2"></a>
+						<span>Man</span>
+					</li>
+				
+					<li class="" role="tab">
+						<a href="#slide3"></a>
+						<span>Jesus</span>
+					</li>
+				
+					<li class="" role="tab">
+						<a href="#slide4"></a>
+						<span>The Holy Spirit</span>
+					</li>
+				
+					<li class="" role="tab">
+						<a href="#slide5"></a>
+						<span>Salvation</span>
+					</li>
+				
+					<li class="" role="tab">
+						<a href="#slide6"></a>
+						<span>Eternity</span>
+					</li>
+				
+					<li class="" role="tab">
+						<a href="#slide7"></a>
+						<span>The Bible</span>
+					</li>
+								</ul>
+			
+	
+				<div class="slides slides--about row">
+					<div class="slides__bg" style="background-image:url(http://oasisla.flywheelsites.com/wp-content/uploads/2015/11/About_Beliefs_700x700.jpg);"></div>
+
+
+										
+						<div id="slide1" class="slide slide--fade animated animated-duration-500 tab--open fadeIn" style="display: block;">
+							
+							<div class="slide__content content-rte">
+								<div>
+									<h4>God is the Creator and Ruler of the universe. He has eternally existed in three personalities revealed to us as: the Father, the Son, and the Holy Spirit. These three are co-equal and are one God.</h4>
+<p><a href="https://www.bible.com/bible/111/gen.1.1,26-27.niv" target="_blank">Genesis 1:1, 26-27</a> and <a href="https://www.bible.com/bible/111/gen.3.22.niv" target="_blank">3:22</a><br>
+<a href="https://www.bible.com/bible/111/psa.90.2.niv" target="_blank">Psalm 90:2</a><br>
+<a href="https://www.bible.com/bible/111/mat.28.19.niv" target="_blank">Matthew 28:19</a><br>
+<a href="https://www.bible.com/bible/111/1pe.1.2.niv" target="_blank">1 Peter 1:2</a><br>
+<a href="https://www.bible.com/bible/111/2co.13.14.niv" target="_blank">2 Corinthians 13:14</a><br>
+<a href="https://www.bible.com/bible/111/1jn.5.7.niv" target="_blank">1 John 5:7</a></p>
+								</div>
+							</div>
+						</div>
+										
+						<div id="slide2" class="slide slide--fade animated animated-duration-500 fadeIn" style="display: none;">
+							
+							<div class="slide__content content-rte">
+								<div>
+									<h4>Man is made in the spiritual image of God, to be like Him in character. He is the supreme object of God’s creation. Although man has tremendous potential for good, he is marred by an attitude of disobedience called “sin”. This attitude separates man from God. We believe that God has a destiny for each believer and that His purpose for us involves the establishing of His Kingdom in our lives.</h4>
+<p><a href="https://www.bible.com/bible/111/gen.1.27.niv" target="_blank">Genesis 1:27</a><br>
+<a href="https://www.bible.com/bible/111/psa.8.3-6.niv" target="_blank">Psalm 8:3-6</a><br>
+<a href="https://www.bible.com/bible/111/isa.53.6.niv" target="_blank">Isaiah 53:6a</a><br>
+<a href="https://www.bible.com/bible/111/rom.3.23.niv" target="_blank">Romans 3:23</a><br>
+<a href="https://bible.com/111/isa.59.1-2.niv" target="_blank">Isaiah 59:1-2</a><br>
+<a href="https://bible.com/111/jhn.15.16.niv" target="_blank">John 15:16</a><br>
+<a href="https://bible.com/111/eph.2.10.niv" target="_blank">Ephesians 2:10</a></p>
+								</div>
+							</div>
+						</div>
+										
+						<div id="slide3" class="slide slide--fade animated animated-duration-500 fadeIn" style="display: none;">
+							
+							<div class="slide__content content-rte">
+								<div>
+									<h4>Jesus Christ is the Son of God. He is co-equal with the Father. Jesus lived a sinless human life, and offered Himself as the perfect sacrifice for the sins of all men by dying on a cross. He arose from the dead after three days to demonstrate His power over sin and death. He ascended to Heaven’s glory, and will return again to earth to reign as King of Kings, and Lord of Lords.</h4>
+<p><a href="https://www.bible.com/bible/111/isa.9.6.niv" target="_blank">Isaiah 9:6</a><br>
+<a href="https://www.bible.com/bible/111/jhn.1.1-4,14.niv" target="_blank">John 1:1-4, 14</a><br>
+<a href="https://www.bible.com/bible/111/jhn.14.10-30.niv" target="_blank">John 14:10-30</a><br>
+<a href="https://www.bible.com/bible/111/heb.4.14-15.niv" target="_blank">Hebrews 4:14-15</a><br>
+<a href="https://www.bible.com/bible/111/1co.15.3-4.niv" target="_blank">1 Corinthians 15:3-4</a><br>
+<a href="https://www.bible.com/bible/111/2co.5.14-15.niv" target="_blank">2 Corinthians 5:14-15</a><br>
+<a href="https://www.bible.com/bible/111/mat.24.30.niv" target="_blank">Matthew 24:30</a><br>
+<a href="https://www.bible.com/bible/111/rev.1.7.niv" target="_blank">Revelation 1:7</a><br>
+<a href="https://www.bible.com/bible/111/rom.1.3-4.niv" target="_blank">Romans 1:3-4</a><br>
+<a href="https://www.bible.com/bible/111/act.1.9-11.niv" target="_blank">Acts 1:9-11</a><br>
+<a href="https://www.bible.com/bible/111/tit.2.13.niv" target="_blank">Titus 2:13</a></p>
+								</div>
+							</div>
+						</div>
+										
+						<div id="slide4" class="slide slide--fade animated animated-duration-500 fadeIn" style="display: none;">
+							
+							<div class="slide__content content-rte">
+								<div>
+									<h4>The Holy Spirit is equal with the Father and the Son as God. He is present in the world to make men aware of their need for Jesus Christ. He also lives in every Christian from the moment of salvation. He provides the Christian with power for living, understanding of spiritual truth, and guidance in doing what is right. The Christian seeks to live under His control daily. We believe that the baptism in the Holy Spirit is an endowment of power available to every believer subsequent to conversion. We believe in the present supernatural ministry of the Holy Spirit through the believers by the manifestation of His gifts.</h4>
+<p><a href="https://www.bible.com/bible/111/2co.3.17.niv" target="_blank">2 Corinthians 3:17</a><br>
+<a href="https://www.bible.com/bible/111/jhn.16.7-13.niv" target="_blank">John 16:7-13</a><br>
+<a href="https://www.bible.com/bible/111/jhn.14.16-17.niv" target="_blank">John 14:16-17, 26</a><br>
+<a href="https://www.bible.com/bible/111/jhn.15.26.niv" target="_blank">John 15:26</a><br>
+<a href="https://www.bible.com/bible/111/act.1.8.niv" target="_blank">Acts 1:8</a><br>
+<a href="https://www.bible.com/bible/111/1co.2.12.niv" target="_blank">1 Corinthians 2:12</a>, <a href="https://www.bible.com/bible/111/1co.3.16.niv" target="_blank">3:16</a>, <a href="https://www.bible.com/bible/111/1co.12.7-11,28.niv" target="_blank">12:7-11, 28</a><br>
+<a href="https://www.bible.com/bible/111/eph.1.13.niv" target="_blank">Ephesians 1:13</a>, <a href="https://www.bible.com/bible/111/eph.5.18.niv" target="_blank">5:18</a>, <a href="https://www.bible.com/bible/111/eph.4.7-16.niv" target="_blank">4:7-16</a><br>
+<a href="https://www.bible.com/bible/111/gal.5.22-25.niv" target="_blank">Galatians 5:22-25</a><br>
+<a href="https://www.bible.com/bible/111/luk.24.49.niv" target="_blank">Luke 24:49</a><br>
+<a href="https://www.bible.com/bible/111/act.2.4.niv" target="_blank">Acts 2:4</a><br>
+<a href="https://www.bible.com/bible/111/rom.12.6-8.niv" target="_blank">Romans 12:6-8</a></p>
+								</div>
+							</div>
+						</div>
+										
+						<div id="slide5" class="slide slide--fade animated animated-duration-500 fadeIn" style="display: none;">
+							
+							<div class="slide__content content-rte">
+								<div>
+									<h4>Salvation is a gift from God to man. Man can never make up for his sin by self-improvement or good works. Only by trusting in Jesus Christ as God’s offer of forgiveness can man be saved from sin’s penalty. Eternal life begins the moment one receives Jesus Christ into his life by confession of faith in His life, death, and resurrection.</h4>
+<p><a href="https://www.bible.com/bible/111/rom.6.23.niv" target="_blank">Romans 6:23</a><br>
+<a href="https://www.bible.com/bible/111/eph.2.8-9.niv" target="_blank">Ephesians 2:8-9</a><br>
+<a href="https://www.bible.com/bible/111/jhn.14.6.niv" target="_blank">John 14:6</a>, <a href="https://www.bible.com/bible/111/jhn.1.12.niv" target="_blank">1:12</a><br>
+<a href="https://www.bible.com/bible/111/tit.3.5.niv" target="_blank">Titus 3:5</a><br>
+<a href="https://www.bible.com/bible/111/gal.3.26.niv" target="_blank">Galatians 3:26</a><br>
+<a href="https://www.bible.com/bible/111/rom.5.1.niv" target="_blank">Romans 5:1</a><br>
+<a href="https://www.bible.com/bible/111/rom.10.9-10.niv" target="_blank">Romans 10:9-10</a></p>
+								</div>
+							</div>
+						</div>
+										
+						<div id="slide6" class="slide slide--fade animated animated-duration-500 fadeIn" style="display: none;">
+							
+							<div class="slide__content content-rte">
+								<div>
+									<h4>Man was created to exist forever. He will either exist eternally separated from God by sin, or in union with God through forgiveness and salvation. To be eternally separated from God is Hell. To be eternally in union with Him is eternal life. Heaven and Hell are places of eternal existence.</h4>
+<p><a href="https://www.bible.com/bible/111/jhn.3.16.niv" target="_blank">John 3:16</a><br>
+<a href="https://www.bible.com/bible/111/1jn.2.25.niv" target="_blank">1 John 2:25</a>, <a href="https://www.bible.com/bible/111/1jn.5.11-13.niv" target="_blank">5:11-13</a><br>
+<a href="https://www.bible.com/bible/111/act.24.15.niv" target="_blank">Acts 24:15</a><br>
+<a href="https://www.bible.com/bible/111/rom.6.23.niv" target="_blank">Romans 6:23</a><br>
+<a href="https://www.bible.com/bible/111/rev.20.15.niv" target="_blank">Revelation 20:15</a><br>
+<a href="https://www.bible.com/bible/111/mat.18.8.niv" target="_blank">Matthew 18:8</a><br>
+<a href="https://www.bible.com/bible/111/mat.25.44-46.niv" target="_blank">Matthew 25:44-46</a><br>
+<a href="https://www.bible.com/bible/111/dan.12.2.niv" target="_blank">Daniel 12:2</a></p>
+								</div>
+							</div>
+						</div>
+										
+						<div id="slide7" class="slide slide--fade animated animated-duration-500 fadeIn" style="display: none;">
+							
+							<div class="slide__content content-rte">
+								<div>
+									<h4>The Bible is God’s Word to all men. It was written by human authors, under the supernatural guidance of the Holy Spirit. It is the supreme source of truth for Christian beliefs and living. Because it is inspired by God, it is truth without any mixture of error, relevant to contemporary society, and carries power to impart faith in life.</h4>
+<p><a href="https://www.bible.com/bible/111/2ti.3.16.niv" target="_blank">2 Timothy 3:16</a><br>
+<a href="https://www.bible.com/bible/111/2pe.1.20-21.niv" target="_blank">2 Peter 1:20-21</a><br>
+<a href="https://www.bible.com/bible/111/psa.119.105,160.niv" target="_blank">Psalm 119:105, 160</a><br>
+<a href="https://www.bible.com/bible/111/psa.19.7-11.niv" target="_blank">Psalm 19:7-11</a><br>
+<a href="https://www.bible.com/bible/111/pro.30.5.niv" target="_blank">Proverbs 30:5</a><br>
+<a href="https://www.bible.com/bible/111/heb.4.12.niv" target="_blank">Hebrews 4:12</a><br>
+<a href="https://www.bible.com/bible/111/rom.10.17.niv" target="_blank">Romans 10:17</a></p>
+								</div>
+							</div>
+						</div>
+									</div>
+			</div>
