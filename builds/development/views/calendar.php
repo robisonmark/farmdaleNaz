2a7e1a9ed92253b4361701d366dfd42c84fb4037
Matthew Robison <Matthew Robison>
2016-06-22 08:34:28 -0500

    php

---
+++ builds/development/views/calendar.php
@@ -0,0 +1,23 @@
+<div class="calendar">
+    <table class="calendar">
+        <thead>
+            <td></td>
+            <td></td>
+            <td></td>
+        </thead>
+        <tbody>
+            <tr class="daysHeader">
+                <td>Sun</td>
+                <td>Mon</td>
+                <td>Tue</td>
+                <td>Wed</td>
+                <td>Thu</td>
+                <td>Fri</td>
+                <td>Sat</td>
+            </tr>
+
+        </tbody>
+    </table>
+</div>
+
+<div>
---
+++ builds/development/views/header.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="dcterms.created" content="Thu, 23 October 2014 02:33:32 GMT">
+		<meta name="description" content="To make Christlike Disciples in the Nations we are Connecting with God, Each Other and our Community" />
+		<meta name="keywords" content="Farmdale, Nazarene, God, Louisville, Kentucky, Jesus, Connecting" />
+		<meta name="author" content="Robros">
+		<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+
+		<title>
+        	Farmdale Nazarene - Louisville, Kentucky - Connecting
+        </title>
+        <link rel="stylesheet" href="./css/normalize.css">
+        <link rel="stylesheet" href="./css/style.css">
+        <script src="https://use.fontawesome.com/72047c876c.js"></script>
+        <script src="http://code.jquery.com/jquery-1.12.0.min.js"></script>
+        
+	</head>
+<!-- END HEAD -->
+
+    <body>
+    <!-- Landing Page -->
+    <header id="intro">
+     <!--Begin Nav Bar -->
+        <nav id="nav">
+            <section class="navbar">
+                <div class="brand">
+                    <a href="#welcome">
+                        <img id="logo" src="./images/farmdaleLogo.svg" alt="farmdale logo">
+                        <img class="script" src="./images/farmdaleScript.svg" alt="farmdale wordmark">
+                    </a>
+                </div>
+
+                <div class="button_container" id="toggle"> 
+                    <span class="top"></span>
+                    <span class="middle"></span>
+                    <span class="bottom"></span>
+                </div>
+
+                <div class="overlay" id="overlay">
+                    <div class="overlay-menu">
+                        <ul>
+                            <li><a class="welcome" href="#welcome">I'm New</a></li>
+                            <li><a class="events" href="#events">Events</a></li>
+                            <li><a class="ministries" href="#ministries">Connect</a></li>
+                            <li><a class="about" href="#about">About Us</a></li>
+                            <li><a class="staff" href="#staff">Staff</a></li> 
+                        </ul>
+                    </div>
+                </div>
+            </section>
+        </nav> 
+        <!-- End Nav Bar -->
